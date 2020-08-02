815df34e0c11b7d323be4ff2c6065177fb2c19c4
Aleksandr Osadchiy <alex.o@blaize.tech>
2020-08-02 20:58:16 +0300

    add about template

---
+++ template-parts/content-about.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package munich
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php munich_post_thumbnail(); ?>
+
+	<div class="entry-content">
+		<div class="textPage">
+			<div><?php the_field( 'left_column' ); ?></div>
+			<div><?php the_field( 'right_column' ); ?></div>
+		</div>
+	</div><!-- .entry-content -->
+
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+			edit_post_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'my-site' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
+</article><!-- #post-<?php the_ID(); ?> -->
