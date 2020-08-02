f2b727d6cf7e1cf879428013026c9576f2c09cf4
Aleksandr Osadchiy <alex.o@blaize.tech>
2020-08-02 19:23:13 +0300

    add contacts template

---
+++ page-contacts.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Template Name: Contacts page
+*/
+get_header();
+?>
+
+	<div id="primary" class="content-area subpage">
+		<main id="main" class="site-main">
+			<div class="container">
+				<?php if(function_exists('bcn_display')) { 
+					echo '<div class="breadcrumbs"><ul>';
+						bcn_display(); 
+					echo '</ul></div>';
+					}
+				?>
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', 'contacts' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+// get_sidebar();
+get_footer();
---
+++ template-parts/content-contacts.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package munich
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="contacts-page">
+
+		<h2><?php the_field( 'page_title' ); ?></h2>
+		<div class="top_side">
+			<div class="map">
+				<?php the_field( 'map_frame' ); ?>
+			</div>
+			<?php if ( have_rows( 'contacts_block' ) ) : ?>
+				<?php while ( have_rows( 'contacts_block' ) ) : the_row(); ?>
+						<div class="contacts-block">
+							<ul>
+								<li><span>adress</span><?php the_sub_field( 'adress' ); ?></li>
+								<li><span>phone</span><a href="fax:<?php the_sub_field( 'phone' ); ?>"><?php the_sub_field( 'phone' ); ?></a></li>
+								<li><span>phone</span><a href="fax:<?php the_sub_field( 'phone' ); ?>"><?php the_sub_field( 'phone' ); ?></a></li>
+								<li><span>fax</span><a href="fax:<?php the_sub_field( 'fax' ); ?>"><?php the_sub_field( 'fax' ); ?></a></li>
+							</ul>
+						</div>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+
+		<div class="contact-form">
+			<h2><?php the_field( 'contacts_form_title' ); ?></h2>
+			<?php
+				$shortcode = get_post_meta($post->ID,'contacts_form_-_shortcode',true);
+				echo do_shortcode($shortcode);
+ 			?>
+		</div>
+	
+	</div>
+
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+			edit_post_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'my-site' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
+</article><!-- #post-<?php the_ID(); ?> -->
